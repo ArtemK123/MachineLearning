00f6eda826f55911aa8616cb52f9cdfccfce9241
ArtemK123 <darkmode1012@gmail.com>
2020-10-11 14:00:40 +0300

    Added .net implementation of lab1

---
+++ lab1/DotNetImplementation/MLApp/MLApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.ML;
+using MLAppML.Model;
+
+namespace MLApp
+{
+    internal class Program
+    {
+        private const string ModelPath = @"C:\Users\Artem\AppData\Local\Temp\MLVSTools\MLAppML\MLAppML.Model\MLModel.zip";
+
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+
+            // Load model
+            ITransformer mlModel = mlContext.Model.Load(ModelPath, out _);
+
+            PredictionEngine<ModelInput, ModelOutput> predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            ModelInput modelInput = new ModelInput { SentimentText = "HIGHLIGHTS VIDEO ON YOUTUBE" };
+
+            ModelOutput modelOutput = predEngine.Predict(modelInput);
+
+            Console.WriteLine(modelOutput.Prediction);
+        }
+    }
+}
