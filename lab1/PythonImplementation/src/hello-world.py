9352b5c2a93893ac1823ab09b9091961e4cdcc18
ArtemK123 <darkmode1012@gmail.com>
2020-10-15 10:27:03 +0300

    Added matplotlib and seaborn references

--- lab1/PythonImplementation/src/hello-world.py
+++ lab1/PythonImplementation/src/hello-world.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+df = pd.read_csv('../resources/wikipedia-detox-250-line-data.tsv', sep='\t')
+
+df['Sentiment'].hist()
