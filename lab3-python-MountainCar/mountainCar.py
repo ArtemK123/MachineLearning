129046c419ae7a46e4812fb3cdf858f10c533932
ArtemK123 <darkmode1012@gmail.com>
2020-11-29 13:38:25 +0200

    Reimplemented algorithm. Used discrete state count. Used linear changing epsilon

--- lab3-python-MountainCar/mountainCar.py
+++ lab3-python-MountainCar/mountainCar.py
@@ -3,21 +3,22 @@ import math
 import random
 
 env = gym.make('MountainCar-v0') # https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
-EPISODES_COUNT = 100
-MAX_STEPS_COUNT = 200  # maximum in 200 steps is built into the env
-LEARNING_FACTOR = 1 # learning factor
-DISCOUNT_FACTOR = 1 # discount factor
-EPSILON_GREEDY_FACTOR = 0 # epsilon-greedy factor 
-RANDOM_Q_VALUE_MINIMUM = -10
-RANDOM_Q_VALUE_MAXIMUM = 10
+EPISODES_COUNT = 5000
+LEARNING_FACTOR = 0.2 # learning factor
+DISCOUNT_FACTOR = 0.9 # discount factor
+EPSILON_GREEDY_FACTOR_MINIMUM = 0
+EPSILON_GREEDY_FACTOR_MAXIMUM = 0.8
+RANDOM_Q_VALUE_MINIMUM = -1
+RANDOM_Q_VALUE_MAXIMUM = 1
+ROUND_DIGITS = 3
+STATE_POSITION_BUCKETS = 150
+STATE_VELOCITY_BUCKETS = 120
 
 actions = [
     0, # move left
     1, # stay
     2, # move right
 ]
-
-Q_table = {} # state_key: [action1_q_value, action2_q_value, action3_q_value]
 
 def generate_random_q_values():
     q_values = []
@@ -26,38 +27,32 @@ def generate_random_q_values():
 
     return q_values
 
-def get_q_value(state, action):
-    if (state not in Q_table):
-        Q_table[state] = generate_random_q_values()
-    
-    q_values = Q_table[state]
+def get_q_value(q_table, state, action):
+    q_values = q_table[state]
     return q_values[action]
 
-def set_q_value(state, action, value):
-    if (state not in Q_table):
-        Q_table[state] = generate_random_q_values()
-
-    q_values = Q_table[state]
+def set_q_value(q_table, state, action, value):
+    q_values = q_table[state]
     q_values[action] = value
 
-def get_max_q_value(state):
+def get_max_q_value(q_table, state):
     max_q_value = -math.inf
     for action in actions:
-        current_q_value = get_q_value(state, action) 
+        current_q_value = get_q_value(q_table, state, action) 
         if (current_q_value > max_q_value):
             max_q_value = current_q_value
     return max_q_value
 
-def get_next_action(state):
-    if random.random() < EPSILON_GREEDY_FACTOR:
+def get_next_action(q_table, state, epsilon):
+    if random.random() < epsilon:
         return get_random_action()
-    return get_optimal_action(state)
+    return get_optimal_action(q_table, state)
 
-def get_optimal_action(state):
+def get_optimal_action(q_table, state):
     optimal_action = -1
     optimal_action_q_value = -math.inf
     for action in actions:
-        q_value = get_q_value(state, action)
+        q_value = get_q_value(q_table, state, action)
         if (q_value > optimal_action_q_value):
             optimal_action = action
             optimal_action_q_value = q_value
@@ -67,36 +62,79 @@ def get_random_action():
     return random.randint(0, len(actions) - 1)
 
 def get_state(observation):
-    car_position, car_velosity = observation
-    round_digits = 3
-    return f'{round(car_position, round_digits)}, {round(car_velosity, round_digits)}'
+    car_position_raw, car_velosity_raw = observation
+    return get_state_from_velocity_and_position(normalize_car_position(car_position_raw), normalize_car_velocity(car_velosity_raw))
 
-def update_q_value(state, action, reward, next_state):
-    current_value = get_q_value(state, action)
-    new_value = current_value + LEARNING_FACTOR * (reward + DISCOUNT_FACTOR * get_max_q_value(next_state) - current_value)
-    set_q_value(state, action, new_value)
+def normalize_car_position(position_raw):
+    position_min = env.observation_space.low[0]
+    position_max = env.observation_space.high[0]
 
-def run_episode():
+    step = (position_max - position_min) / STATE_POSITION_BUCKETS
+
+    return round((position_raw - position_min) / step)
+
+def normalize_car_velocity(velocity_raw):
+    velocity_min = env.observation_space.low[1]
+    velocity_max = env.observation_space.high[1]
+
+    step = (velocity_max - velocity_min) / STATE_VELOCITY_BUCKETS
+
+    return round((velocity_raw - velocity_min) / step)
+
+def update_q_value(q_table, state, action, reward, next_state):
+    current_value = get_q_value(q_table, state, action)
+    new_value = current_value +\
+                                LEARNING_FACTOR * (
+                                    reward
+                                    + DISCOUNT_FACTOR * get_max_q_value(q_table, next_state)
+                                    - current_value)
+    set_q_value(q_table, state, action, new_value)
+
+def create_q_table():
+    new_q_table = {}
+    
+    for velocity in range(STATE_VELOCITY_BUCKETS):
+        for position in range(STATE_POSITION_BUCKETS):
+            state = get_state_from_velocity_and_position(velocity, position)
+            new_q_table[state] = generate_random_q_values()
+
+    return new_q_table
+
+def get_state_from_velocity_and_position(velocity, position):
+    return velocity * STATE_POSITION_BUCKETS + position
+
+q_table = create_q_table()
+
+epsilon = EPSILON_GREEDY_FACTOR_MAXIMUM
+epsilon_decay = (EPSILON_GREEDY_FACTOR_MAXIMUM - EPSILON_GREEDY_FACTOR_MINIMUM) / EPISODES_COUNT
+
+for i_episode in range(EPISODES_COUNT):
     observation = env.reset()
     previous_state = get_state(observation)
     previous_action = 1
-    for _ in range(MAX_STEPS_COUNT):
-        env.render()
+    done = False
+    total_reward = 0
 
-        current_action = get_next_action(previous_state)
+    while not done:
+        if i_episode > EPISODES_COUNT - 50:
+            env.render()
+
+        current_action = get_next_action(q_table, previous_state, epsilon)
         observation, reward, done, info = env.step(current_action)
-
-        if done:
-            break
 
         current_state = get_state(observation)
 
-        update_q_value(previous_state, previous_action, reward, current_state)
+        if done and reward != -1:
+            set_q_value(q_table, previous_state, previous_action, reward)
+        else:
+            update_q_value(q_table, previous_state, previous_action, reward, current_state)
         previous_action = current_action
         previous_state = current_state
+        total_reward += reward 
+    
+    if (i_episode % 100 == 0):
+        print(f'Episode number: {i_episode}, TotalReward: {total_reward}')
 
-for i_episode in range(EPISODES_COUNT):
-    print(f'Episode number: {i_episode}')
-    run_episode()
+    epsilon -= epsilon_decay
 
 env.close()
