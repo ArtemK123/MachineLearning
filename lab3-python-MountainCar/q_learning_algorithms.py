f3748cdcde793a4e9d2c26696bb1f44040738056
ArtemK123 <darkmode1012@gmail.com>
2020-12-03 01:01:52 +0200

    Created class QTable

--- lab3-python-MountainCar/q_learning_algorithms.py
+++ lab3-python-MountainCar/q_learning_algorithms.py
@@ -6,34 +6,34 @@ import q_table as q_table_module
 def get_max_q_value(q_table, state): # max
     max_q_value = -math.inf
     for action in constants.ACTIONS:
-        current_q_value = q_table_module.get_q_value(q_table, state, action) 
+        current_q_value = q_table.get_value(state, action) 
         if (current_q_value > max_q_value):
             max_q_value = current_q_value
     return max_q_value
 
-def get_optimal_action(q_table, state): # argmax
-    optimal_action = -1
-    optimal_action_q_value = -math.inf
-    for action in constants.ACTIONS:
-        q_value = q_table_module.get_q_value(q_table, state, action)
-        if (q_value > optimal_action_q_value):
-            optimal_action = action
-            optimal_action_q_value = q_value
-    return optimal_action
-
 def get_next_action(q_table, state, epsilon):
     if random.random() < epsilon:
-        return get_random_action()
-    return get_optimal_action(q_table, state)
-
-def get_random_action():
-    return random.randint(0, len(constants.ACTIONS) - 1)
+        return _get_random_action()
+    return _get_optimal_action(q_table, state)
 
 def update_q_value(q_table, state, action, reward, next_state):
-    current_value = q_table_module.get_q_value(q_table, state, action)
+    current_value = q_table.get_value(state, action)
     new_value = current_value +\
                                 constants.LEARNING_FACTOR * (
                                     reward
                                     + constants.DISCOUNT_FACTOR * get_max_q_value(q_table, next_state,)
                                     - current_value)
-    q_table_module.set_q_value(q_table, state, action, new_value)
+    q_table.set_value(state, action, new_value)
+
+def _get_optimal_action(q_table, state): # argmax
+    optimal_action = -1
+    optimal_action_q_value = -math.inf
+    for action in constants.ACTIONS:
+        q_value = q_table.get_value(state, action)
+        if (q_value > optimal_action_q_value):
+            optimal_action = action
+            optimal_action_q_value = q_value
+    return optimal_action
+
+def _get_random_action():
+    return random.randint(0, len(constants.ACTIONS) - 1)
--- lab3-python-MountainCar/q_table.py
+++ lab3-python-MountainCar/q_table.py
@@ -2,25 +2,29 @@ import constants
 import state_space
 import random
 
+class QTable:
+    def __init__(self, table):
+      self.table = table
+  
+    def get_value(self, state, action):
+        q_values = self.table[state]
+        return q_values[action]
+
+    def set_value(self, state, action, value):
+        q_values = self.table[state]
+        q_values[action] = value
+
 def create_q_table():
-    new_q_table = {}
+    new_table = {}
     
     for velocity in range(constants.STATE_VELOCITY_BUCKETS):
         for position in range(constants.STATE_POSITION_BUCKETS):
             state = state_space.compose_state(position, velocity)
-            new_q_table[state] = generate_random_q_values()
+            new_table[state] = _generate_random_actions_array()
 
-    return new_q_table
+    return QTable(new_table)
 
-def get_q_value(q_table, state, action):
-    q_values = q_table[state]
-    return q_values[action]
-
-def set_q_value(q_table, state, action, value):
-    q_values = q_table[state]
-    q_values[action] = value
-
-def generate_random_q_values():
+def _generate_random_actions_array():
     q_values = []
     for _ in range(len(constants.ACTIONS)):
         q_values.append(random.randint(constants.RANDOM_Q_VALUE_MINIMUM, constants.RANDOM_Q_VALUE_MAXIMUM))
