fc8285eaf072e70ced98ae869167ce502c385327
ArtemK123 <darkmode1012@gmail.com>
2020-12-03 00:51:01 +0200

    Moved algorithms to separate file

--- lab3-python-MountainCar/mountainCar.py
+++ lab3-python-MountainCar/mountainCar.py
@@ -4,43 +4,9 @@ import random
 import constants
 import state_space
 import q_table as q_table_module
+import q_learning_algorithms
 
 env = gym.make('MountainCar-v0') # https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
-
-def get_max_q_value(q_table, state): # max
-    max_q_value = -math.inf
-    for action in constants.ACTIONS:
-        current_q_value = q_table_module.get_q_value(q_table, state, action) 
-        if (current_q_value > max_q_value):
-            max_q_value = current_q_value
-    return max_q_value
-
-def get_optimal_action(q_table, state): # argmax
-    optimal_action = -1
-    optimal_action_q_value = -math.inf
-    for action in constants.ACTIONS:
-        q_value = q_table_module.get_q_value(q_table, state, action)
-        if (q_value > optimal_action_q_value):
-            optimal_action = action
-            optimal_action_q_value = q_value
-    return optimal_action
-
-def get_next_action(q_table, state, epsilon):
-    if random.random() < epsilon:
-        return get_random_action()
-    return get_optimal_action(q_table, state)
-
-def get_random_action():
-    return random.randint(0, len(constants.ACTIONS) - 1)
-
-def update_q_value(q_table, state, action, reward, next_state):
-    current_value = q_table_module.get_q_value(q_table, state, action)
-    new_value = current_value +\
-                                constants.LEARNING_FACTOR * (
-                                    reward
-                                    + constants.DISCOUNT_FACTOR * get_max_q_value(q_table, next_state,)
-                                    - current_value)
-    q_table_module.set_q_value(q_table, state, action, new_value)
 
 def get_heuristic_reward(observation, reward):
     car_position_raw, car_velosity_raw = observation
@@ -71,14 +37,14 @@ for i_episode in range(constants.EPISODES_COUNT):
         if i_episode > constants.EPISODES_COUNT - constants.EVALUATION_EPISODES_COUNT:
             env.render()
 
-        current_action = get_next_action(q_table, previous_state, epsilon)
+        current_action = q_learning_algorithms.get_next_action(q_table, previous_state, epsilon)
         observation, reward, done, info = env.step(current_action)
 
         current_state = state_space.get_state(observation, env)
 
         heuristic_reward = get_heuristic_reward(observation, reward)
 
-        update_q_value(q_table, previous_state, previous_action, heuristic_reward, current_state)
+        q_learning_algorithms.update_q_value(q_table, previous_state, previous_action, heuristic_reward, current_state)
 
         previous_action = current_action
         previous_state = current_state
