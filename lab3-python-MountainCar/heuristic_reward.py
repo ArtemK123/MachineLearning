8759df018830fd7e6ab8a88fe6951040c15c525a
ArtemK123 <darkmode1012@gmail.com>
2020-12-03 02:02:23 +0200

    Used only current observation in heuristic reward. Increased efficient of heuristic reward

--- lab3-python-MountainCar/heuristic_reward.py
+++ lab3-python-MountainCar/heuristic_reward.py
@@ -1,17 +1,15 @@
 import constants
 import state_space
 
-def get_reward(observation, previous_observation, reward):
+def get_reward(observation, reward):
     if (reward == 0): # won the game
         return 10000
 
     position_raw, velocity_raw = observation
-    prev_position_raw, prev_velocity_raw = previous_observation
-
     velocity_reward = round(abs(velocity_raw)*100)
 
     if (position_raw < -0.10 or position_raw > 0.2):
-        if (position_raw > prev_position_raw):
+        if (velocity_raw > 0):
             return velocity_reward + 5
         else:
             return velocity_reward - 1
